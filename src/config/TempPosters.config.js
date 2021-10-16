41d14eeaf1e6fdc082507c1a67a07a30b289a250
mitangpatel7234 <mitangpatel7234@gmail.com>
2021-10-16 13:51:44 +0530

    book my show

---
+++ src/config/TempPosters.config.js
@@ -0,0 +1,118 @@
+const PremierImages = [
+    {
+      src: "https://wickedhorror.com/wp-content/uploads/2016/02/The-Conjuring-2-Poster-1.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:…g,ox-322,oy-20/et00312484-ehedpyzcmm-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+    {
+      src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-NzAlICA3ayB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70/et00122455-jnauuzubwb-portrait.jpg",
+      alt: "Conjuring",
+      title: "The Conjuring",
+      subtitle: "Horror/Thriller"
+    },
+  ]
+  
+  export default PremierImages;
